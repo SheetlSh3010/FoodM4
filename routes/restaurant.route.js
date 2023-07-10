3e34a0f80ae377a0a255b23f640de9991b31e44b
SheetlSh3010 <sheetalshar3010@gmail.com>
2023-07-10 14:37:24 +0530

    deply

---
+++ routes/restaurant.route.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const restRouter = express.Router();
+const { RestaurantModel } = require("../models/restaurant.model");
+restRouter.use(express.json());
+
+// add resturant
+
+restRouter.post("/create", async (req, res) => {
+  const payload = req.body;
+  try {
+    const info = new RestaurantModel(payload);
+    await info.save();
+    res.send({ msg: "created" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// get resturant
+
+restRouter.get("/restaurants", async (req, res) => {
+  const info = await RestaurantModel.find(req.query);
+  res.send({ msg: "got restaurants", data: info });
+});
+
+// get resturant by ID
+
+restRouter.get("/restaurants/:id", async (req, res) => {
+  const Id = req.params.id;
+  const info = await RestaurantModel.findById({ _id: Id });
+  res.send({ msg: "got restaurants", data: info });
+});
+
+// This endpoint should return the menu of a specific restaurant identified by its ID.
+
+restRouter.get("/restaurants/:id/menu", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const restaurant = await RestaurantModel.findById(id);
+
+    if (!restaurant) {
+      return res.status(404).json({ error: "Restaurant not found" });
+    }
+
+    res.json(restaurant.menu);
+  } catch (error) {
+    console.error("Error retrieving menu:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// adding new itme in menu
+
+restRouter.post("/restaurants/:id/menu", async (req, res) => {
+  let id = req.params.id;
+  console.log(id);
+  try {
+    let data = await RestaurantModel.findById(id);
+    data.menu.push(req.body);
+    await data.save();
+    res.status(201).send("item added in restaurant" + data.name);
+  } catch (error) {
+    res.status(400).send("error adding new itmes restaurant");
+  }
+});
+
+// delete request
+
+
+module.exports = { restRouter };
